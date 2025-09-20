f41e79200fa3b0f08155449d3c0b65430a8a0687
praveen choudhary <pc852180@gmail.com>
2025-09-20 21:17:25 +0530

    Mern crud

--- Client/src/Pages/About.jsx
+++ Client/src/Pages/About.jsx
@@ -1,10 +1,16 @@
 
 import React from 'react'
-
+import img1 from "../assets/h3.avif";
+import img2 from "../assets/h4.avif";
 function About() {
   return (
     <>
     <h1>About page</h1>
+    <img src={img1} alt="frist image" width="900" height="600px" />
+        <br />
+        <hr size="6" color="red" />
+
+        <img src={img2} alt="frist image" width="900" height="600px" />
     </>
   )
 }
--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -3,6 +3,12 @@ const Home  = ()=>{
     return(
         <>
         <h1>Home page</h1>
+        <img src="h1.jpg" alt="frist image" width="900" height="600px" />
+        <br />
+        <hr size="6" color="red" />
+
+        <img src="h2.jpg" alt="frist image" width="900" height="600px" />
+
         </>
     )
 }
