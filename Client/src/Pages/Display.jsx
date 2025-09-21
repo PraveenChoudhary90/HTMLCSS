0ee4915a6ccb9125d8f5a4f14d60c252e165bbe2
praveen choudhary <pc852180@gmail.com>
2025-09-21 18:17:53 +0530

    Mern

--- Client/src/Pages/Display.jsx
+++ Client/src/Pages/Display.jsx
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Table from 'react-bootstrap/Table';
+
 
 const Display = ()=>{
     const [mydata, setMydata] = useState([]);
@@ -15,11 +17,48 @@ const Display = ()=>{
         LoadData();
     },[]);
 
-    
+  let count = 0;
+    const ans = mydata.map(key=>{
+        count++;
+        return(
+            <>
+            <tr>
+                <td>{count}</td>
+                <td>
+                    <img src={`http://localhost:8000/${key.defaultImage}`} alt="" width="150px" height="100px" />
+                </td>
+                <td>{key.name}</td>
+                <td>{key.email}</td>
+                <td>{key.city}</td>
+                <td>{key.number}</td>
+                <td>{key.state}</td>
+                <td>Delete</td>
+                <td>Update</td>
+            </tr>
+            </>
+        )
+    })
 
     return(
         <>
-        
+         <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Count</th>
+          <th>Image</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>City</th>
+          <th>Number</th>
+          <th>State</th>
+          <th>Delete</th>
+          <th>Update</th>
+        </tr>
+      </thead>
+      <tbody>
+        {ans}
+      </tbody>
+      </Table>
         </>
     )
 }
